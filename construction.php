b5d7fcedc4ac33527e0460d95d15d43f296e854d
Felipe Tavares <felipefiel@yahoo.com.br>
2020-04-05 12:10:54 -0300

    PDO

---
+++ construction.php
@@ -0,0 +1,17 @@
+<?php 
+class Endereco{
+
+	private $rua;
+	private $numero;
+
+	public function __construct(){
+		echo "construtor<br><BR>";
+	}
+}
+
+
+$meuEndereco = new Endereco();
+
+var_dump($meuEndereco);
+
+ ?>
