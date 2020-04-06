18dad2371021627b7e9d4c922470acb6b2df8055
Felipe Tavares <felipefiel@yahoo.com.br>
2020-04-06 14:15:04 -0300

    Organizando as classes e add classe Usuario

---
+++ dao/class/Sql.php
@@ -0,0 +1,40 @@
+<?php 
+class Sql extends PDO {
+
+	private $conn;
+
+	public function __construct(){
+
+		$this->conn = new PDO ("mysql:host=localhost;dbname=dbphp7", "root", "");
+	}
+	private function setParams($stmt, $params = array()){
+		foreach ($params as $key => $value) {
+			$this->setParams($key, $value);
+		}
+	}
+	private function setParam($stmt, $key, $value){
+		$stmt->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array()){
+
+		$stmt = $this->conn->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+		return $stmt;
+	
+	}
+
+	public function select($rawQuery, $params = array()):array{
+
+		$stmt = $this->query($rawQuery, $params);
+		return $stmt->fetchall(PDO::FETCH_ASSOC);
+
+	}
+
+
+}
+
+
+ ?>
